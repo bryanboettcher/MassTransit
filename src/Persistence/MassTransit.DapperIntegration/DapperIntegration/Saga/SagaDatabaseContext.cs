d5837f0cc851743ed689bb56f1b488a98c5870f1
Bryan Boettcher <bryan.boettcher@gmail.com>
2025-04-22 12:00:51 -0500

    continuing versioning support

--- src/Persistence/MassTransit.DapperIntegration/DapperIntegration/Saga/SagaDatabaseContext.cs
+++ src/Persistence/MassTransit.DapperIntegration/DapperIntegration/Saga/SagaDatabaseContext.cs
@@ -9,14 +9,13 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
-using Internals;
 using Microsoft.Data.SqlClient;
 
 /// <summary>
 /// Contains saga-specific logic as well as respecting ISagaVersion
 /// </summary>
 /// <typeparam name="TSaga"></typeparam>
-public sealed class SagaDatabaseContext<TSaga> : DatabaseContext<TSaga>
+public sealed class SagaDatabaseContext<TSaga> : DatabaseContext<TSaga>, SqlBuilder<TSaga>
     where TSaga : class, ISaga
 {
     readonly SqlConnection _connection;
@@ -36,46 +35,67 @@ public sealed class SagaDatabaseContext<TSaga> : DatabaseContext<TSaga>
         _versionColumnName = GetColumnName(typeof(TSaga), nameof(ISagaVersion.Version));
     }
     
-    public async Task DeleteAsync(TSaga instance, CancellationToken cancellationToken)
-    {
-        var param = new DynamicParameters();
-        param.Add(_idColumnName, instance.CorrelationId);
-
-        var sql = $"DELETE FROM {_tableName} WHERE {_idColumnName} = @id";
-
-        if (instance is ISagaVersion versioned)
-        {
-            sql += $" AND {_versionColumnName} = @version";
-            param.Add(_versionColumnName, versioned.Version);
-        }
-
-        var rows = await _connection.ExecuteAsync(sql, param, _transaction);
-
-    }
-
     public Task<TSaga> LoadAsync(Guid correlationId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var param = new DynamicParameters();
+        param.Add("correlationId", correlationId);
+
+        var sql = BuildLoadSql();
+
+        return _connection.QueryFirstOrDefaultAsync<TSaga>(sql, param, _transaction);
     }
 
     public Task<IEnumerable<TSaga>> QueryAsync(Expression<Func<TSaga, bool>> filterExpression, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var sql = BuildQuerySql(filterExpression, out var parameters);
+        
+        return _connection.QueryAsync<TSaga>(sql, parameters, _transaction);
     }
-
+    
     public Task InsertAsync(TSaga instance, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var sql = BuildInsertSql();
+
+
     }
 
-    public Task UpdateAsync(TSaga instance, CancellationToken cancellationToken = default)
+    public async Task UpdateAsync(TSaga instance, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var sql = BuildUpdateSql();
+        
+        var param = new DynamicParameters();
+        param.Add("correlationId", instance.CorrelationId);
+
+        if (instance is ISagaVersion versioned)
+            param.Add("version", versioned.Version);
+
+        var rows = await _connection.ExecuteAsync(sql, instance, _transaction);
+        if ((rows == 0) == (instance is ISagaVersion))
+        {
+            throw new DapperConcurrencyException("Saga Update failed", typeof(TSaga), instance.CorrelationId);
+        }
+    }
+    
+    public async Task DeleteAsync(TSaga instance, CancellationToken cancellationToken)
+    {
+        var sql = BuildDeleteSql();
+
+        var param = new DynamicParameters();
+        param.Add("correlationId", instance.CorrelationId);
+
+        if (instance is ISagaVersion versioned)
+            param.Add("version", versioned.Version);
+
+        var rows = await _connection.ExecuteAsync(sql, param, _transaction);
+        if ((rows == 0) == (instance is ISagaVersion))
+        {
+            throw new DapperConcurrencyException("Saga Delete failed", typeof(TSaga), instance.CorrelationId);
+        }
     }
 
     public void Commit()
     {
-        throw new NotImplementedException();
+        _transaction?.Commit();
     }
 
     public ValueTask DisposeAsync()
@@ -109,10 +129,66 @@ public sealed class SagaDatabaseContext<TSaga> : DatabaseContext<TSaga>
         if (property is null) // the hell?
             return propertyName;
 
+        return GetColumnName(type, property);
+    }
+
+    static string GetColumnName(Type type, PropertyInfo property)
+    {
         var columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
         if (columnAttribute is null || string.IsNullOrEmpty(columnAttribute.Name))
-            return propertyName;
+            return property.Name;
 
         return columnAttribute.Name;
     }
+
+    static string BuildUpdateExpression(Type sagaType)
+    {
+        var expressions =
+            from prop in sagaType.GetProperties()
+            let columnName = GetColumnName(sagaType, prop)
+            let propertyName = prop.Name
+            select $"[{columnName}] = @{propertyName}";
+
+        return string.Join(',', expressions);
+    }
+
+    string BuildLoadSql()
+    {
+        return $"SELECT * FROM {_tableName} WITH (UPDLOCK, ROWLOCK) WHERE {_idColumnName} = @correlationId";
+    }
+
+    string BuildQuerySql(Expression<Func<TSaga, bool>> filterExpression, out DynamicParameters parameters)
+    {
+        (var whereStatement, parameters) = WhereStatementHelper.GetWhereStatementAndParametersFromExpression(filterExpression);
+
+        return $"SELECT * FROM {_tableName} WITH (UPDLOCK, ROWLOCK) {whereStatement}";
+    }
+
+    string BuildUpdateSql()
+    {
+        var updateExpression = BuildUpdateExpression(typeof(TSaga));
+
+        var sql = $"UPDATE {_tableName} SET {updateExpression} WHERE {_idColumnName} = @correlationId";
+
+        if (typeof(ISagaVersion).IsAssignableFrom(typeof(TSaga)))
+            sql += $" AND {_versionColumnName} = @version";
+
+        return sql;
+    }
+
+    string BuildDeleteSql()
+    {
+        var sql = $"DELETE FROM {_tableName} WHERE {_idColumnName} = @correlationId";
+
+        if (typeof(ISagaVersion).IsAssignableFrom(typeof(TSaga)))
+            sql += $" AND {_versionColumnName} = @version";
+
+        return sql;
+    }
+
+}
+
+
+public interface SqlBuilder<TModel> where TModel : class
+{
 }
